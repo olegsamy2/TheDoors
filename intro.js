fdbc5c8f036bcba3a994602b0ee0866207f9e4ec
olegsamy2 <109589138+olegsamy2@users.noreply.github.com>
2022-10-26 19:58:52 +0300

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,11 @@
+﻿function intro(){
+const name = prompt("What's your name?","");
+if (name) {
+let answer =`Glad to see you, ${name}`;
+alert(answer);
+return answer;
+} else {
+ answer = "What a pity, you didn't introduce yourself";
+alert(answer);
+return answer;
+}}
